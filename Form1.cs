6cccf0fd266a6599361a50ceaa7ca24d5297db2b
qiujoe <qiujinglei@gmail.com>
2015-09-09 16:09:43 +0800

    init

---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Stock.Trader.WeiTuo;
+using Stock.Trader.WeiTuo.HuaTai;
+
+namespace StockTrader
+{
+    public partial class Form1 : Form
+    {
+        I_StockTrader trader = null;
+        public Form1()
+        {
+            InitializeComponent();
+            trader = new HuaTaiStockTrader();
+            trader.Init();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            trader = new HuaTaiStockTrader();
+            trader.Init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trader.SellStock(textBox1.Text, float.Parse(textBox2.Text), int.Parse(textBox3.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            trader.BuyStock(textBox1.Text, float.Parse(textBox2.Text), int.Parse(textBox3.Text));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            trader.CancelStock(textBox1.Text, float.Parse(textBox2.Text), int.Parse(textBox3.Text));
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trader.GetCashInfo();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            trader.PurchaseFundSZ(textBox1.Text, float.Parse(textBox2.Text));
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            trader.RedempteFundSZ(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            trader.MergeFundSZ(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            trader.PartFundSZ(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            trader.PurchaseFundSH(textBox1.Text, float.Parse(textBox2.Text));
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            trader.RedempteFundSH(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            trader.MergeFundSH(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            trader.PartFundSH(textBox1.Text, int.Parse(textBox3.Text));
+        }
+
+    }
+}
---
+++ XiaDan.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Stock.Trader.WeiTuo;
+
+namespace Stock.Trader
+{
+    /// <summary>
+    /// 这个类的实例是临界资源
+    /// </summary>
+    class XiaDan
+    {
+        #region .ctor()
+        private XiaDan()
+        {
+        }
+        #endregion
+
+        private static XiaDan instance = new XiaDan();
+        I_StockTrader trader;
+
+        public static XiaDan Instance {
+            get { return instance; }
+        }
+
+        public void BuyStock(String code, float price, int num)
+        {
+            // TODO: 检查？分钟内是否重复购买
+            trader.BuyStock(code, price, num);
+            // 保存下单记录到文件
+        }
+
+        public void SellStock(String code, float price, int num)
+        {
+            // TODO: 检查？分钟内是否重复卖出
+            trader.SellStock(code, price, num);
+            // 保存下单记录到文件
+        }
+
+        /// <summary>
+        /// 申购基金
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="total"></param>
+        public void PurchaseFund(String code, float total)
+        {
+        }
+
+        /// <summary>
+        /// 赎回基金
+        /// </summary>
+        /// <param name="code">代码</param>
+        /// <param name="num">份额</param>
+        public void RedempteFund(String code, int num)
+        {
+        }
+
+        /// <summary>
+        /// 合并子基金
+        /// </summary>
+        /// <param name="code">母鸡代码</param>
+        /// <param name="num">合并数量</param>
+        public void MergeFund(String code, int num)
+        {
+        }
+
+        /// <summary>
+        /// 分拆母基金
+        /// </summary>
+        /// <param name="code">母鸡代码</param>
+        /// <param name="num">分拆数量</param>
+        public void PartFund(String code, int num)
+        {
+        }
+        /// <summary>
+        /// 撤销超时未成交股票
+        /// </summary>
+        public void CancelTimeoutStock()
+        {
+            // TODO: 获取未成交数据
+            // 检查是否超时，超时就撤销
+        }
+
+        /// <summary>
+        /// 提交交易记录到服务器
+        /// </summary>
+        public void CommitRecordToServer()
+        {
+            // 读取当日的操作日志文件，分析后上传到服务器
+        }
+    }
+}
